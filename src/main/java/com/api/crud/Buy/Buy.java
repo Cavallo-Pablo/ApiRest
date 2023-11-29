597fbacaf2ec55f60cdcc03fbfd13556434448b0
Cavallo Pablo <112218966+Cavallo-Pablo@users.noreply.github.com>
2023-11-29 17:35:41 -0300

    Actualizacion de buy

--- src/main/java/com/api/crud/Buy/Buy.java
+++ src/main/java/com/api/crud/Buy/Buy.java
@@ -1,17 +1,25 @@
 package com.api.crud.Buy;
 
+import com.api.crud.Travel.Travel;
+import com.api.crud.User.User;
 import jakarta.persistence.Basic;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter @Setter
 public class Buy {
     @Id
     @GeneratedValue
@@ -19,4 +27,11 @@ public class Buy {
     @Basic
     private String amount;
     private String quantity;
+
+    @OneToOne
+    private User user;
+
+    @OneToMany
+    private Travel travel;
+
 }
